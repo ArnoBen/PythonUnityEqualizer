5ad3497c10608fe79b1f1b5b557b60110cb880ec
Arno Benizri <arno.benizri@gmail.com>
2019-08-08 17:08:38 +0200

    Add files via upload

--- Assets/Python/UDPSender.py
+++ Assets/Python/UDPSender.py
@@ -5,4 +5,4 @@ class Sender:
         self.sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
     def send(self, data, adress = "127.0.0.1", port = 9900):
-        self.sock.sendto(json.dumps(data).encode(), ("127.0.0.1",9900))
+        self.sock.sendto(json.dumps(data).encode(), (adress, port))
